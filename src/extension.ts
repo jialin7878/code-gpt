033b15237ba513a7a664c4db90519f79f5b0c6f9
jialin7878 <E0425498@u.nus.edu>
2023-01-15 01:21:28 +0800

    Remove extra comments

--- src/extension.ts
+++ src/extension.ts
@@ -88,11 +88,7 @@ async function generateTestcases(): Promise<string> {
   return output.data.choices[0].text.trim();
 }
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "code-gpt" is now active!');
 
   // Create Webview
@@ -105,9 +101,6 @@ export function activate(context: vscode.ExtensionContext) {
     )
   );
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   let explainCodeCommand = vscode.commands.registerCommand(
     "code-gpt.explainCode",
     async () => {
