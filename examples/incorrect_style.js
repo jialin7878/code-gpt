d0171998005d4216ebf41209946df7b3f3a97f05
jialin7878 <E0425498@u.nus.edu>
2023-01-15 09:17:12 +0800

    Update testcases

---
+++ examples/incorrect_style.js
@@ -0,0 +1,7 @@
+function should_be_camel_case(foo) {
+  const camel_case_variable_name = 96;
+  // Should separate into different lines
+  camel_case_variable_name += 1;
+  camel_case_variable_name -= 1;
+  return camel_case_variable_name;
+}
